d9c5fa60ece7b1b547ab0ab62498f5aa0738cc8d
vaibhavithakare <vaibhavi.thakare@walchandsangli.ac.in>
2026-06-14 17:02:00 +0530

    feat:add basic transform stage

---
+++ src/transform.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def transfrom_data(df):
+    print("Starting transfrom step")
+    
+    df.columns = df.columns.str.lower()
+    df.columns = df.columns.str.replace(" ","_")
+    df.columns = df.columns.str.replcae("-","_")
+
+
+    df("order_date") = pd.to_datetime(df["order_date"],errors="coerce")
+    df["ship_date"] = pd.to_datetime(df["ship_date"],errors="coerce")
+
+    df["postal_code"] = df["postal_code"].fillna("Unknown")
+
+    print("Transform step completed successfully")
+
+    return df 
